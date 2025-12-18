5f6a941f33c9bf537045b7cd5b8d0893247e4476
韶光 <admin@gloryouth.com>
2025-12-18 08:51:57 +0800

    refactor: consolidate synchronization imports and introduce shim module
    Remove the sync module and consolidate synchronization primitives into a new shim module. This change simplifies the import process for both std and no_std environments by providing a unified interface for Arc, Mutex, and other types. The refactor enhances code clarity and maintainability while ensuring compatibility across different feature flags.

---
+++ src/shim.rs
@@ -0,0 +1,62 @@
+#[cfg(not(feature = "loom"))]
+pub use core::cell::Cell;
+#[cfg(feature = "loom")]
+pub use loom::cell::Cell;
+
+// Atomics (Loom vs Core)
+#[cfg(not(feature = "loom"))]
+pub use core::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
+#[cfg(feature = "loom")]
+pub use loom::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
+
+#[cfg(not(feature = "loom"))]
+mod memory {
+    #[cfg(feature = "std")]
+    mod inner {
+        pub use std::boxed::Box;
+        pub use std::collections::VecDeque;
+        pub use std::sync::Arc;
+        pub use std::vec::Vec;
+    }
+
+    #[cfg(not(feature = "std"))]
+    mod inner {
+        pub use alloc::boxed::Box;
+        pub use alloc::collections::VecDeque;
+        pub use alloc::sync::Arc;
+        pub use alloc::vec::Vec;
+    }
+
+    pub use inner::*;
+}
+
+#[cfg(feature = "loom")]
+mod memory {
+    pub use loom::sync::{Arc, Weak};
+    pub use std::boxed::Box;
+    pub use std::collections::VecDeque;
+    pub use std::vec::Vec;
+}
+
+pub use memory::*;
+
+// Lock (Mutex) Abstraction
+#[cfg(feature = "loom")]
+pub use loom::sync::Mutex;
+
+#[cfg(not(feature = "loom"))]
+mod locks {
+    #[cfg(feature = "std")]
+    pub use antidote::Mutex;
+
+    #[cfg(not(feature = "std"))]
+    pub use spin::Mutex;
+}
+
+#[cfg(not(feature = "loom"))]
+pub use locks::*;
+
+// Ensure a compile error in no_std environment without spin feature.
+// 确保在 no_std 且没有 spin 的情况下报错
+#[cfg(all(not(feature = "std"), not(feature = "spin"), not(feature = "loom")))]
+compile_error!("To use swmr-cell in no_std, you must enable the 'spin' feature or 'loom' feature.");
